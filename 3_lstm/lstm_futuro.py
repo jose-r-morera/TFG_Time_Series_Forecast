0fece3170cf365bf0bc449fc07fa7972e64f6bd0
José R. Morera Campos <alu0101471846@ull.edu.es>
2025-04-16 10:41:56 +0100

    Atencion

--- 3_lstm/lstm_futuro.py
+++ 3_lstm/lstm_futuro.py
@@ -36,13 +36,13 @@ if gpus:
 with open(DATA_PATH, "rb") as f:
     data = pickle.load(f)
 
-x_train = data["air_temperature"]["x_train"]
-future_train = data["air_temperature"]["future_train"]
-y_train = data["air_temperature"]["y_train"]
+x_train = data["train"]["air_temperature"]["past_variables"]
+future_train = data["train"]["air_temperature"]["future_variables"]
+y_train = data["train"]["air_temperature"]["y"]
 
-x_val = data["air_temperature"]["x_val"]
-future_val = data["air_temperature"]["future_val"]
-y_val = data["air_temperature"]["y_val"]
+x_val = data["test"]["air_temperature"]["past_variables"]
+future_val = data["test"]["air_temperature"]["future_variables"]
+y_val = data["test"]["air_temperature"]["y"]
 
 #############
 # BATCH AND SHUFFLE
@@ -89,170 +89,87 @@ for batch in dataset_train.take(1):
     inputs, targets = batch
     past_data, future_data = inputs
     
-# Example placeholder values – update these with your actual data dimensions
+# Define the model
 past_data_shape = (past_data.shape[1], past_data.shape[2])
 future_data_shape = (future_data.shape[1], future_data.shape[2])    
-output_dim = targets.shape[1]     # e.g., How many values to predict (e.g., 3-hour forecast)
+target_shape = targets.shape[1]     # e.g., How many values to predict (e.g., 3-hour forecast)
+
+learning_rate = 0.001
 EPOCHS = 700
 
 ####################################################
 
-def build_model(hp):
-    K.clear_session() # clear memory
-    gc.collect() # garbage collector
-
-    ### MODEL DEFINITION ####
-    # General parameters
-    use_second_lstm = hp.Boolean('use_second_lstm', default=False) # Second lstm on past data
-    use_attention_1 = hp.Boolean('use_attention_1', default=False) # Attention on past data
-    use_attention_2 = False # Attention on merged data
-    if not use_attention_1:
-        use_attention_2 = hp.Boolean('use_attention_2', default=False)
-    
-    #################################
-    # --- PAST DATA Input layer --- #
-    #################################
-    past_input = layers.Input(shape=(past_data_shape), name ="past")
-    
-    # --- First LSTM layer ---
-    units_1 = hp.Int('units_1', min_value=16, max_value=640, step=24)
-    bidir_1 = hp.Boolean('bidir_1', default=False)
-    
-    if bidir_1:
-        units_1 //= 2  # Halve the units for bidirectional LSTM
-        past_layers = layers.Bidirectional(
-            layers.LSTM(units_1, return_sequences=use_second_lstm or use_attention_1) 
-        ) (past_input)
-    else:
-        past_layers = layers.LSTM(units_1, return_sequences=use_second_lstm or use_attention_1)(past_input)
-    past_out_dim = units_1 # save past data dimesion
-    
-    # --- Optional second LSTM layer ---
-    if use_second_lstm:
-        units_2 = hp.Int('units_2', min_value=16, max_value=640, step=24)
-        past_out_dim = units_2
-        bidir_2 = hp.Boolean('bidir_2', default=False)
+class Attention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super(Attention, self).__init__()
+        # These are trainable layers that compute the attention score
+        self.W = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
         
-        if bidir_2:
-            past_layers = layers.Bidirectional(layers.LSTM(units_2, return_sequences=use_attention_1))(past_layers)
-        else:
-            past_layers = layers.LSTM(units_2, return_sequences=use_attention_1)(past_layers)
-
-    # --- Optional Attention Layer ---
-    if use_attention_1:
-        if use_second_lstm:
-            attn_units_1 = hp.Int('attn_units_1', min_value=units_2//2, max_value=units_2*2, step=24)
-        else:
-            attn_units_1 = hp.Int('attn_units_1', min_value=units_1//2, max_value=units_1*2, step=24)
-        past_out_dim = attn_units_1
-        past_layers = Attention(attn_units_1)(past_layers)  # Apply attention before dense layer
- 
-    ##################
-    # --- FUTURE --- #
-    ##################
-    future_input = layers.Input(shape=(future_data_shape), name ="future_data")
-
-    units_f = hp.Int('units_f', min_value=16, max_value=640, step=24)
-    bidir_f = hp.Boolean('bidir_f', default=False)
+    def call(self, inputs):
+        # inputs shape: (batch_size, time_steps, hidden_units)
+        # Compute score for each time step
+        score = tf.nn.tanh(self.W(inputs))             # (batch_size, time_steps, units)
+        attention_weights = tf.nn.softmax(self.V(score), axis=1)  # (batch_size, time_steps, 1)
+        # Multiply each hidden state by its attention weight and sum over time steps
+        context_vector = tf.reduce_sum(attention_weights * inputs, axis=1)  # (batch_size, hidden_units)
+        return context_vector
     
-    if bidir_f:
-        units_f //= 2  # Halve the units for bidirectional LSTM
-        future_layers = layers.Bidirectional(
-            layers.LSTM(units_f, name="lstm_future", return_sequences=False) 
-        )(future_input)
-    else:
-        future_layers = layers.LSTM(units_f, name="lstm_future", return_sequences=False)(future_input)
+################################################
 
-    #################
-    # --- MERGE --- #
-    #################
-    merged = layers.Concatenate(name='concatenate')([past_layers, future_layers])
-    merged_dim = past_out_dim + units_f # past + future units
+# Encoder part (LSTM for past data)
+past_data_layer = tf.keras.layers.Input(shape=past_data_shape, name="past_data")
+encoder_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(350, return_sequences=False))(past_data_layer)
+#encoder_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(190))(encoder_lstm)
 
-    if use_attention_2:
-        merged = layers.Reshape((1, -1), name="att_reshape")(merged)
-        attn_units_2 = hp.Int('attn_units_2', min_value=merged_dim//2, max_value=round(merged_dim*1.5), step=24)
-        merged = Attention(attn_units_2)(merged)
-    
-    dense_2 = hp.Boolean('dense_2', default=False)
-    if dense_2: 
-        units_dense_2 = hp.Int('units_dense_2', min_value=merged_dim//4, max_value=round(merged_dim*1.5), step=24)
-        merged = layers.Dense(units_dense_2, activation="relu")(merged)
+# Decoder part (LSTM for future exogenous features)
+future_data_layer = tf.keras.layers.Input(shape=future_data_shape, name="future_data")
+#decoder_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256))(future_data_layer)
+decoder_lstm = tf.keras.layers.LSTM(300)(future_data_layer)
 
-    # --- Dense output layer ---
-    output = layers.Dense(output_dim)(merged)
+# Combine the outputs of encoder and decoder (you can concatenate or merge them)
+merged = tf.keras.layers.concatenate([encoder_lstm, decoder_lstm])
+merged_lstm = tf.keras.layers.Reshape((1, -1))(merged)  # Reshape to (batch, timesteps=1, features) for LSTM
+#merged = tf.keras.layers.LSTM(512)(merged_lstm) # DOES NOT MAKE SENSE AFTER MERGING
+merged = Attention(400)(merged_lstm)
 
-    ########################################################
-    model = keras.Model(inputs=[past_input, future_input], outputs=output, name='past_future_model')
-    # --- Learning rate tuning ---
-    learning_rate = hp.Float('learning_rate', min_value=1e-5, max_value=1e-2, sampling='LOG')
-    
-    model.compile(
-        optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
-        loss='mse',
-        metrics=["mse"],
-    )
-    
-    model.summary()
-    return model
+#merged = tf.keras.layers.Dense(288*2, activation="relu")(merged)
 
-# Create a tuner instance
-tuner = kt.Hyperband(
-    build_model,
-    objective='val_mse',
-    max_epochs=EPOCHS,
-    executions_per_trial=3,
-    factor=3,
-    directory='../output/tuner',
-    project_name='lstm_tuner'
+# Final output layer
+output_units = target_shape # Output shape should match the target sequence
+outputs = tf.keras.layers.Dense(output_units)(merged)
+
+# Create the model
+model = tf.keras.Model(inputs=[past_data_layer, future_data_layer], outputs=outputs)
+
+# Compile the model
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss="mse")
+model.summary()
+
+# Define the callbacks
+path_checkpoint = "lstm_future_checkpoint.weights.h5"
+es_callback = tf.keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=4)
+
+modelckpt_callback = tf.keras.callbacks.ModelCheckpoint(
+    monitor="val_loss",
+    filepath=path_checkpoint,
+    verbose=1,
+    save_weights_only=True,
+    save_best_only=True,
 )
-# tuner = kt.BayesianOptimization(
-#     build_model,
-#     objective='val_mse',
-#     executions_per_trial=3,
-#     directory='../output/tuner',
-#     project_name='lstm_future_bayesian'
-# )
 
-# Define tunable patience and min_delta for ReduceLROnPlateau
-def get_callbacks(hp):
-    patience = hp.Int('reduce_lr_patience', min_value=2, max_value=5, step=1)
-    factor = hp.Choice('reduce_lr_factor', values=[0.4, 0.5, 0.6, 0.7])
+reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor='val_loss', 
+    factor=0.4,         
+    patience=3,         
+    verbose=1,
+    min_lr=1e-7
+)
 
-    reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(
-        min_delta=0.0001,  # Minimum change to be considered an improvement
-        monitor='val_loss', 
-        factor=factor,  # Reduce LR by a factor of 0.5
-        patience=patience,  # Wait for `patience` epochs before reducing
-        verbose=1,
-        min_lr=5e-7
-    )
-    
-    es_callback = tf.keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0.0001, patience=10, restore_best_weights=True)
-
-    return [reduce_lr_callback, es_callback]
-
-
-###############################################################################
-print("Search space hyperparameters:")
-for hp in tuner.oracle.get_space().space:
-    print(f"  {hp.name}: {getattr(hp, 'values', getattr(hp, 'sampling', ''))}")
-
-# Run the hyperparameter search
-tuner.search(dataset_train, epochs=EPOCHS, validation_data=dataset_val, callbacks=get_callbacks(tuner.oracle.hyperparameters))
-
-# Print best hyperparameters found
-best_hp = tuner.get_best_hyperparameters(num_trials=1)[0]
-print("Best hyperparameters:")
-print(f"First LSTM units: {best_hp.get('units_1')}")
-print(f"First LSTM bidirectional: {best_hp.get('bidir_1')}")
-print(f"Use second LSTM layer: {best_hp.get('use_second_lstm')}")
-if best_hp.get('use_second_lstm'):
-    print(f"Second LSTM units: {best_hp.get('units_2')}")
-    print(f"Second LSTM bidirectional: {best_hp.get('bidir_2')}")
-print(f"Use Attention: {best_hp.get('use_attention_1')}")
-if best_hp.get('use_attention_1'):
-    print(f"Attention layer units: {best_hp.get('attn_units_1')}")
-print(f"Learning rate: {best_hp.get('learning_rate')}")
-print(f"Reduce LR patience: {best_hp.get('reduce_lr_patience')}")
-print(f"Reduce LR factor: {best_hp.get('reduce_lr_factor')}")
+# Train the model
+history = model.fit(
+    dataset_train,
+    epochs=EPOCHS,
+    validation_data=dataset_val,
+    callbacks=[modelckpt_callback, reduce_lr_callback, es_callback],
+)
--- 3_lstm/paquetes.py
+++ 3_lstm/paquetes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
+import random
 
 # VARIABLES #
 FILES = ["grafcan_arona_features.csv", "grafcan_la_laguna_features.csv", "grafcan_la_orotava_features.csv", 
@@ -15,26 +16,42 @@ STEP = 6 # Number of time steps to skip between each input sequence
 OUTPUT_PATH = "paquetes_futuro_s6.pkl"
 
 ####################################################################################################
-def create_df_windows(df, features, target):
+def create_df_windows(df, features, target, train_split=0.85):
   label_start = past_n
   y = df.iloc[label_start:][target]
 
-  past_variables = []
-  future_variables = []
-  y_multi_step = []
-  for i in range(0, len(df) - past_n - future_n + 1, STEP):
-    past_variables_window = df.iloc[i:i + past_n][features].values
-    past_variables.append(past_variables_window)
-    future_n_window = df.iloc[label_start + i:label_start + i + future_n][features].drop(columns=[target]).values
-    future_variables.append(future_n_window)
-    y_window = y.iloc[i:i + future_n].values
-    y_multi_step.append(y_window)
-
-  past_variables = np.array(past_variables)
-  future_variables = np.array(future_variables)
-  y_multi_step = np.array(y_multi_step)
-    
-  return past_variables, future_variables, y
+  train_past_variables, test_past_variables = [], []
+  train_future_variables, test_future_variables = [], []
+  train_y, test_y = [], []
+  i = 0
+  while (i  < len(df) - past_n - future_n + 1):
+    if random.random() < train_split: 
+      train_past_variables.append(df.iloc[i:i + past_n][features].values)
+      future_n_window = df.iloc[label_start + i:label_start + i + future_n][features].drop(columns=[target]).values
+      train_future_variables.append(future_n_window)
+      train_y.append(y.iloc[i:i + future_n].values)
+      i += STEP
+    else: 
+      # Test data
+      test_past_variables.append(df.iloc[i:i + past_n][features].values)
+      future_n_window = df.iloc[label_start + i:label_start + i + future_n][features].drop(columns=[target]).values
+      test_future_variables.append(future_n_window)
+      test_y.append(y.iloc[i:i + future_n].values)
+      # Avoid train / test data leakage
+      i += past_n + future_n 
+      
+  train = {
+    "past": np.array(train_past_variables),
+    "future": np.array(train_future_variables),
+    "y": np.array(train_y)
+  }
+  test = {
+    "past": np.array(test_past_variables),
+    "future": np.array(test_future_variables),
+    "y": np.array(test_y)
+  }
+      
+  return train, test
 #####################################################################################################
   
 
@@ -50,16 +67,15 @@ for file in FILES:
   
   for target in targets:
     features = ["sin_day", "cos_day", "sin_year", "cos_year", target]
-    past_variables, future_variables, y = create_df_windows(df, features, target)
+    train, test = create_df_windows(df, features, target)
     # Store data
     data[target][file] = {}
-    data[target][file]["past_variables"] = past_variables
-    data[target][file]["future_variables"] = future_variables
-    data[target][file]["y"] = y
+    data[target][file]["train"] = train
+    data[target][file]["test"] = test
 
-####################
-# Train-test split #
-####################
+#######################
+# Dataset Aggregation #
+#######################
 train_data = {}
 test_data = {}
 template = {
@@ -74,47 +90,42 @@ for target in targets:
   test_data[target] =  {key: [] for key in template}
   # Test has 10% of the data in each dataset source
   for dataset, samples in data[target].items():
-    print("samples length: ", len(samples["past_variables"]))
-    # Split data
-    TRAIN_PERCENTAGE = 0.9
-    train_split = int(len(samples["past_variables"]) * TRAIN_PERCENTAGE)
-    train_data[target]["past_variables"].extend(samples["past_variables"][:train_split])
-    train_data[target]["future_variables"].extend(samples["future_variables"][:train_split])
-    train_data[target]["y"].extend(samples["y"][:train_split])
+    train_data[target]["past_variables"].extend(samples["train"]["past"])
+    train_data[target]["future_variables"].extend(samples["train"]["future"])
+    train_data[target]["y"].extend(samples["train"]["y"])
     # Test data
-    test_data[target]["past_variables"].extend(samples["past_variables"][train_split:])
-    test_data[target]["future_variables"].extend(samples["future_variables"][train_split:])
-    test_data[target]["y"].extend(samples["y"][train_split:])
-  
+    test_data[target]["past_variables"].extend(samples["test"]["past"])
+    test_data[target]["future_variables"].extend(samples["test"]["future"])
+    test_data[target]["y"].extend(samples["test"]["y"])
+
 print("Train data shape:")
 for target in targets:
-  print(f"{target}: {len(train_data[target]['past_variables'])} samples")
-print("Test data shape:")
+  # Convert to numpy array
+  train_data[target]["past_variables"] = np.array(train_data[target]["past_variables"])
+  train_data[target]["future_variables"] = np.array(train_data[target]["future_variables"])
+  train_data[target]["y"] = np.array(train_data[target]["y"])
+  # Test
+  test_data[target]["past_variables"] = np.array(test_data[target]["past_variables"])
+  test_data[target]["future_variables"] = np.array(test_data[target]["future_variables"])
+  test_data[target]["y"] = np.array(test_data[target]["y"])
+  print(f"{target}: {train_data[target]['past_variables'].shape} samples")
+  print(f"TEST {target}: {test_data[target]['past_variables'].shape} samples")
+
+# Normalization
 for target in targets:
-  print(f"{target}: {len(test_data[target]['past_variables'])} samples")
-  
-###################################################
-def normalize(train, test, no_normalize):
-  data_mean = train.mean(axis=0)
-  data_std = train.std(axis=0)
-  return [(train - data_mean) / data_std, data_mean, data_std]
-#################################################
+  # Mean across every window and every timestep
+  mean = train_data[target]["past_variables"].mean(axis=(0,1))
+  std = train_data[target]["past_variables"].std(axis=(0,1))
+  train_data[target]["past_variables"] = (train_data[target]["past_variables"] - mean) / std
+  test_data[target]["past_variables"] = (test_data[target]["past_variables"] - mean) / std
+  # Last feature is the target variable
+  train_data[target]["y"] = (train_data[target]["y"] - mean[-1]) / std[-1]
+  test_data[target]["y"] = (test_data[target]["y"] - mean[-1]) / std[-1]
+  # Save data
+  train_data[target]["mean"] = mean
+  train_data[target]["std"] = std
 
-# no_normalize = ["sin_day", "cos_day", "sin_year", "cos_year"]
-
-
-#   # Normalization #
-#   # (After data split)
-#   features = df.drop(columns=["time", *no_normalize]).astype("float32")
-#   normalized_features, mean, std = normalize(features.values, first_2025_indepast_variables)
-#   normalized_features = pd.DataFrame(normalized_features, columns=features.columns)
-  
-#     for col in no_normalize:
-#       normalized_features[col] = df[col].astype("float32")
-  
-#       data = {"mean": mean, "std": std} # Save data statistics from normaliation
-
-
-
-# with open(OUTPUT_PATH, "wb") as f:
-#     pickle.dump(data, f)
+# Save train and test data
+with open(OUTPUT_PATH, "wb") as f:
+  pickle.dump({"train": train_data, "test": test_data}, f)
+print(f"Data saved to {OUTPUT_PATH}")
