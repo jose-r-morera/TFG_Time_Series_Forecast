3d76ff9bb899d90e7b11e25d21812066f69cca7f
José R. Morera Campos <alu0101471846@ull.edu.es>
2025-05-24 04:55:48 +0100

    Memory finished initial version

--- 3_data_windows/create_validation_windows.py
+++ 3_data_windows/create_validation_windows.py
@@ -21,19 +21,19 @@ import matplotlib.pyplot as plt
 # FILES = {"punta_hidalgo": ["grafcan_punta_hidalgo_features.csv"]}
 # FILES = {"punta_hidalgo": ["openmeteo_punta_hidalgo_features.csv"],}
 
-# FILES = { "santa_cruz": ["grafcan_santa_cruz_features.csv", "openmeteo_santa_cruz_features.csv"]}
-FILES = {"garachico": ["grafcan_garachico_features.csv", "openmeteo_garachico_features.csv"]}
+FILES = { "santa_cruz": ["grafcan_santa_cruz_features.csv", "openmeteo_santa_cruz_features.csv"]}
+# FILES = {"garachico": ["grafcan_garachico_features.csv", "openmeteo_garachico_features.csv"]}
 
 DATASETS_PATH = "../1_data_preprocessing/test_data/"
 
 #PAST_N = 13 # Number of past time steps to use as input
-FUTURE_N = 6 # Number of future time steps to predict
+FUTURE_N = 12 # Number of future time steps to predict
 STEP = 6 # Number of time steps to skip between each input sequence
 
 USE_COVARIATES = True # Use other features as covariates (ej, temperature and pressure to predict humidity)
 
 # Output is stored as JSON
-OUTPUT_PATH = "f6_val/paquetes_s6_cov_full_garachico_"
+OUTPUT_PATH = "f12_val/paquetes_s6_cov_full_santa_cruz_"
 
 # p24 (hum)
 # MEAN = {"air_temperature": [ 69.98326441, 983.1485558,   20.08620014],
@@ -67,9 +67,27 @@ OUTPUT_PATH = "f6_val/paquetes_s6_cov_full_garachico_"
 #################################################################################
 # F12 #
 # p24 (hum)
-MEAN = {"air_temperature": [ 69.98326441, 983.1485558,   20.08620014],
-    "relative_humidity": [ 20.25431473, 983.35715617,  69.96161102],
-    "atmospheric_pressure": [ 20.28514145,  69.76048701, 983.32293809]}
+# MEAN = {"air_temperature": [ 69.95251366 ,983.14983971  ,20.07069291],
+#     "relative_humidity": [ 20.26555542, 983.42008363 , 69.97553217],
+#     "atmospheric_pressure": [ 20.31185008 , 69.71633929, 983.33650912]}
+# STD = {
+#     "air_temperature": [ 16.5270088 , 36.13966174 , 4.03835471],
+#     "relative_humidity": [ 4.01288743 ,36.00349708 ,17.06842685],
+#     "atmospheric_pressure": [4.04800907 ,17.0079241  ,36.15540983]}
+# # p17 (temp)
+# MEAN  = {"air_temperature": [ 70.02335191, 983.14956786 , 20.07664523],
+#     "relative_humidity": [ 20.25115927 ,983.35973308 , 69.9497285],
+#     "atmospheric_pressure": [ 20.28367824  ,69.757629  , 983.31318897]}
+# STD = {"air_temperature": [16.49223533, 36.14928115,  4.03222583],
+#     "relative_humidity": [ 4.01023079 ,35.99421295 ,17.05460965],
+#     "atmospheric_pressure": [ 4.05767129 ,17.01128025, 36.11040093]}
+# # p20 (pres)
+MEAN = {"air_temperature": [ 69.96751293, 983.15113898 , 20.06486167],
+    "relative_humidity": [ 20.2647646 , 983.42288888  ,69.95440903],
+    "atmospheric_pressure": [ 20.30942994  ,69.70872055 ,983.33026966]}     
+STD = {    "air_temperature": [16.52675168 ,36.14401336 , 4.04187231],
+    "relative_humidity": [ 4.01527812 ,36.00133956 ,17.08855661],
+    "atmospheric_pressure": [4.05254614 ,17.03091489, 36.15583918]}
 ####################################################################################################
 def df_raw_windows(df, past_features, future_features, target, past_n, future_n, step):
   past_variables_windows = []
--- 4_lstm/lstm_futuro_b.py
+++ 4_lstm/lstm_futuro_b.py
@@ -19,9 +19,9 @@ from custom_attention import CustomAttention
 # DATA_PATH = "../3_data_windows/f6/paquetes_s6_cov_arona_orotava_p17.pkl"
 # DATA_PATH = "../3_data_windows/f6/paquetes_s6_cov_arona_laguna_orotava_p17.pkl"
 
-DATA_PATH = "../3_data_windows/f3/paquetes_s6_cov_full_p2.pkl"
+DATA_PATH = "../3_data_windows/f12/paquetes_s6_cov_full_p17.pkl"
 
-DATASET = "relative_humidity"  # atmospheric_pressure or relative_humidity or air_temperature
+DATASET = "air_temperature"  # atmospheric_pressure or relative_humidity or air_temperature
 
 BATCH_SIZE = 64
 SHUFFLE = True
@@ -94,21 +94,20 @@ def build_and_train_model(dataset_train):
     future_in = tf.keras.layers.Input(shape=future_shape, name="future_data")
 
     # Past data 
-    past_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(42, return_sequences=False))(past_in)
-    past_lstm = tf.keras.layers.Dense(20)(past_lstm)
+    past_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(65, return_sequences=False))(past_in)
+    past_lstm = tf.keras.layers.Dense(65)(past_lstm)
     
-    # Future exogenous features
-    future_dense = tf.keras.layers.Flatten()(future_in)
-    future_dense = tf.keras.layers.Dense(4)(future_dense)
+    # Future
+    decoder_lstm = tf.keras.layers.LSTM(4, return_sequences=False)(future_in)
 
-    # Combine the outputs of past and future
+    # Combine the outputs of encoder and decoder (you can concatenate or merge them)
     future_residue = tf.keras.layers.Flatten()(future_in)
-    merged = tf.keras.layers.concatenate([past_lstm, future_dense, future_residue])
+    merged = tf.keras.layers.concatenate([past_lstm, decoder_lstm, future_residue])
 
     # Final output layer
-    merged = tf.keras.layers.Dense(6* target_dim)(merged) 
-    out = tf.keras.layers.Dense(target_dim)(merged)
-
+    # merged = tf.keras.layers.Dense(6* target_dim)(merged)
+    out= tf.keras.layers.Dense(target_dim)(merged)
+    
     model = tf.keras.Model([past_in, future_in], out)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss="mse")
     
